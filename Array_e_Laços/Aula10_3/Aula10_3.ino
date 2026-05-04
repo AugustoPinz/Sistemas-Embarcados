7f1e62e02f78bee9625b2125afc5c18530e6effa
AugustoPinz <bredlaupinzaugusto@gmail.com>
2026-05-04 15:05:18 -0300

    Update Aula10_3.ino

--- Array_e_Laços/Aula10_3/Aula10_3.ino
+++ Array_e_Laços/Aula10_3/Aula10_3.ino
@@ -3,15 +3,12 @@ void setup() {
  
   for(int i=0;i<5;i++){
     pinMode(leds[i], OUTPUT);
-  }
-  
-}
-void loop() {
-  for(int i=0;i<5;i++){
     digitalWrite(leds[i], HIGH);
     delay(500);
     digitalWrite(leds[i], LOW);
     delay(500);
   }
-    
+  
+}
+void loop() {   
 }
